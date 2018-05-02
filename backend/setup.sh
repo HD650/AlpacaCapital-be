6b2eb4181a8199514666907ba91547ab3d23f094
HD650 <unrealcat@outlook.com>
2018-05-02 19:19:35 -0400

    Setup documentation.

---
+++ backend/setup.sh
@@ -0,0 +1,24 @@
+# compiler
+sudo yum install gcc gcc-c++ automake autoconf
+# install python 3.5
+wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+tar xvf Python-3.5.2.tgz
+./configure
+make -j 10
+make altinstall
+# shutdown firewall
+sudo service firewalld stop
+# install database
+sudo yum install mariadb
+sudo yum install mariadb-devel
+# essential python packages
+pip3 install mysqlclient
+pip3 install django
+# setup mysql
+sudo service mysqld start
+mysql_secure_installation
+# insert data
+mysql -u xxx -p < backup.sql
+# setup django and run
+python3 ./manage.py migrate
+python3 ./manage.py runserver 0.0.0.0:8000
