f5753bcd2cb88852f37c504e9ef63e382c0c8e83
Cloud User <ec2-user@ip-172-31-17-247.us-east-2.compute.internal>
2018-08-25 20:01:03 +0000

    Add backend admin site

--- backend/new_db/admin.py
+++ backend/new_db/admin.py
@@ -1,3 +1,98 @@
 from django.contrib import admin
+from new_db.models import IndustrialSector, Person, IpProtection, FdaApprovalStatus, CoreTechnology, Product, Market, Funding, OpenFunding, Company, TeamMember, TargetMarket
 
-# Register your models here.
+
+class TargetMarketInline(admin.StackedInline):
+    model = TargetMarket
+    extra = 1
+
+
+class TeamMemberInline(admin.StackedInline):
+    model = TeamMember
+    extra = 1
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    filter_horizontal = ('core_technology', 'product', 'financial')
+    inlines = (TargetMarketInline, TeamMemberInline)
+
+
+admin.site.register(Company, CompanyAdmin)
+
+
+class IndustrialSectorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(IndustrialSector, IndustrialSectorAdmin)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Person, PersonAdmin)
+
+
+class IpProtectionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(IpProtection, IpProtectionAdmin)
+
+
+class FdaApprovalStatusAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(FdaApprovalStatus, FdaApprovalStatusAdmin)
+
+
+class CoreTechnologyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(CoreTechnology, CoreTechnologyAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class MarketAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Market, MarketAdmin)
+
+
+class FundingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Funding, FundingAdmin)
+
+
+class OpenFundingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(OpenFunding, OpenFundingAdmin)
+
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TeamMember, TeamMemberAdmin)
+
+
+class TargetMarketAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TargetMarket, TargetMarketAdmin)
---
+++ backend/run.sh
@@ -0,0 +1,1 @@
+sudo /usr/local/bin/python3 manage.py runserver 0.0.0.0:80
