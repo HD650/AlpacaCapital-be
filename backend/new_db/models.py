6b22f64346067fb6b73aa26f7a28ea693197b1cf
HD650 <zzhang63@ncsu.edu>
2018-08-29 12:25:36 -0400

    Add new table and API

--- backend/new_db/models.py
+++ backend/new_db/models.py
@@ -15,7 +15,7 @@ class IndustrialSector(models.Model):
 
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
-    name_of_major_contact = models.CharField(max_length=512, blank=True, null=True)
+    name = models.CharField(max_length=512, blank=True, null=True)
     title = models.CharField(max_length=512, blank=True, null=True)
     email = models.CharField(max_length=512, blank=True, null=True)
     phone_number = models.CharField(max_length=512, blank=True, null=True)
@@ -23,7 +23,7 @@ class Person(models.Model):
     linkedin = models.CharField(max_length=512, blank=True, null=True)
 
     def __str__(self):
-        return self.name_of_major_contact
+        return self.name
 
     class Meta:
         managed = True
@@ -42,7 +42,7 @@ class IpProtection(models.Model):
         db_table = 'ip_protection'
 
 
-class FdaApprovalStatus(models.Model):
+class FDAApprovalStatus(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=512, blank=True, null=True)
 
@@ -58,7 +58,7 @@ class CoreTechnology(models.Model):
     id = models.AutoField(primary_key=True)
     technology_name = models.CharField(max_length=512, blank=True, null=True)
     technology_description = models.TextField(blank=True, null=True)
-    problems_to_solve = models.TextField(blank=True, null=True)
+    problems_the_technology_solves = models.TextField(blank=True, null=True)
     ip_protection = models.OneToOneField(IpProtection, on_delete=models.CASCADE)
     fda_approval_status = models.OneToOneField(FdaApprovalStatus, on_delete=models.CASCADE)
 
@@ -100,7 +100,7 @@ class Market(models.Model):
         db_table = 'market'
 
 
-class Funding(models.Model):
+class FundingStage(models.Model):
     id = models.AutoField(primary_key=True)
     round_name = models.CharField(max_length=512, blank=True, null=True)
     date_closed = models.DateField(blank=True, null=True)
@@ -110,17 +110,17 @@ class Funding(models.Model):
     attorney = models.CharField(max_length=512, blank=True, null=True)
 
     def __str__(self):
-        return self.round_name
+        return '%s %s' % (str(self.round_name), str(self.date_closed))
 
     class Meta:
         managed = True
-        db_table = 'funding'
+        db_table = 'founding_stage'
 
 
 class OpenFunding(models.Model):
     id = models.AutoField(primary_key=True)
     round_name = models.CharField(max_length=512, blank=True, null=True)
-    estimated_money_valuation = models.IntegerField(blank=True, null=True)
+    estimated_pre_money_valuation = models.IntegerField(blank=True, null=True)
     amount_of_capital_seeking = models.IntegerField(blank=True, null=True)
     amount_of_equity_selling = models.FloatField(blank=True, null=True)
     projected_monthly_burn_rate = models.IntegerField(blank=True, null=True)
@@ -128,11 +128,25 @@ class OpenFunding(models.Model):
     exit_strategy = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.round_name
+        return '%s %s' % (str(self.round_name), str(self.estimated_pre_money_valuation))
 
     class Meta:
         managed = True
         db_table = 'open_funding'
+
+
+class OfftakeAgreements(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.IntegerField(blank=True, null=True)
+    buyer = models.OneToOneField('Company', on_delete=models.CASCADE, blank=True, null=True)
+    delivery_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s %s " % (str(self.buyer.company_name), str(self.amount), str(self.delivery_date))
+
+    class Meta:
+        managed = True
+        db_table = 'offtake_agreements'
 
 
 # Create your models here.
@@ -142,23 +156,24 @@ class Company(models.Model):
     company_name = models.CharField(max_length=512)
     company_logo = models.CharField(max_length=1024, blank=True, null=True)
     company_website = models.CharField(max_length=512, blank=True, null=True)
-    industrial_sector = models.OneToOneField(IndustrialSector, on_delete=models.CASCADE)
+    industrial_sector = models.OneToOneField(IndustrialSector, on_delete=models.CASCADE, blank=True, null=True)
     two_lines_company_summary = models.TextField(blank=True, null=True)
     # founding_stage (calculate in the runtime)
-    found_time = models.IntegerField(blank=True, null=True)
-    employee_number = models.IntegerField(blank=True, null=True)
-    product_service_development_status = models.CharField(max_length=128, blank=True, null=True)
+    year_founded = models.IntegerField(blank=True, null=True)
+    number_of_employees = models.IntegerField(blank=True, null=True)
+    product_or_service_development_status = models.CharField(max_length=128, blank=True, null=True)
+    product_TRI = models.IntegerField(blank=True, null=True)
 
     # Contact information
-    contact_information = models.OneToOneField(Person, on_delete=models.CASCADE, related_name='contact_person')
+    contact_information = models.OneToOneField(Person, on_delete=models.CASCADE, related_name='contact_person', blank=True, null=True)
 
     # Team
-    team = models.ManyToManyField(Person, through='TeamMember')
+    team = models.ManyToManyField(Person, through='TeamMember', blank=True, null=True)
 
     # TPM
-    core_technology = models.ManyToManyField(CoreTechnology)
-    product = models.ManyToManyField(Product)
-    market = models.ManyToManyField(Market, through='TargetMarket')
+    core_technology = models.ManyToManyField(CoreTechnology, blank=True, null=True)
+    product = models.ManyToManyField(Product, blank=True, null=True)
+    market = models.ManyToManyField(Market, through='TargetMarket', blank=True, null=True)
 
     # Strategy
     value_proposition = models.TextField(blank=True, null=True)
@@ -167,9 +182,11 @@ class Company(models.Model):
     strategy_for_setting_barriers = models.TextField(blank=True, null=True)
 
     # Financial
-    financial = models.ManyToManyField(Funding)
-    current_round = models.OneToOneField(OpenFunding, on_delete=models.CASCADE, null=True)
+    financial = models.ManyToManyField(Funding, blank=True, null=True)
+    current_round = models.OneToOneField(OpenFunding, on_delete=models.CASCADE, blank=True, null=True)
     # total_amount_raised (calculate in the runtime)
+
+    offtake_agreements = models.ManyToManyField(OfftakeAgreements, blank=True, null=True)
 
     # Related files
     executive_summary = models.CharField(max_length=1024, blank=True, null=True)
@@ -184,6 +201,9 @@ class Company(models.Model):
     class Meta:
         managed = True
         db_table = 'company'
+        permissions = (('visitor', 'see basic info'),
+                       ('membership', 'see most info'),
+                       ('admin', 'see all info'))
 
 
 class TeamMember(models.Model):
@@ -194,7 +214,7 @@ class TeamMember(models.Model):
     title = models.CharField(max_length=512, blank=True, null=True)
 
     def __str__(self):
-        return self.person.name
+        return '%s %s %s\%' % (str(self.person.name), str(self.title), str(self.share))
 
     class Meta:
         managed = True
--- backend/new_db/views.py
+++ backend/new_db/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.db import connection
+from django.shortcuts import HttpResponse
+import json
+import pymysql
+from django.shortcuts import redirect
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+one_page = 50
+
+
+def index_page(request):
+    return redirect('/AlpacaCapital-fe/index.html')
+
+
+def log_in(request):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        passwd = request.POST.get("password", "")
+        # no data in the form, try body
+        if email == "" or passwd == "":
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            email = body['email']
+            passwd = body['password']
+        if email == "" or passwd == "":
+            return HttpResponse("FAIL")
+        login_user = authenticate(username=email, password=passwd)
+        if login_user is not None:
+            if login_user.is_active:
+                request.session.set_expiry(86400)
+                login(request, login_user)
+                return HttpResponse("OK")
+    return HttpResponse("FAIL")
+
+
+def log_out(request):
+    try:
+        logout(request)
+        return HttpResponse("OK")
+    except Exception:
+        return HttpResponse("FAIL")
+
+
+def new_user(request):
+    if request.method == "POST":
+        email = request.POST.get("email", "")
+        passwd = request.POST.get("password", "")
+        # no data in the form, try body
+        if email == "" or passwd == "":
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            email = body['email']
+            passwd = body['password']
+        if email == "" or passwd == "":
+            return HttpResponse("FAIL")
+        if User.objects.filter(username=email).exists():
+            return HttpResponse("ALREADY EXISTS")
+        new_created_user = User.objects.create_user(username=email, password=passwd)
+
+        if new_created_user is not None:
+            return HttpResponse("OK")
+        else:
+            return HttpResponse("FAIL")
+
+
+def user_group(request):
+    if request.method == "POST":
+        user = request.user
+        my_group = Group.objects.get(name='my_group_name')
+        my_group.user_set.add(user)
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("FAIL")
+
+
+def user_test(request):
+    if request.user.is_authenticated:
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("FAIL")
+
+
+def get_company_list(request):
+    page_num = request.GET.get('page', 1)
+    page_num = int(page_num)
+    try:
+    
+        json_raw["amount"] = company_num[0][0]
+
+        json_res = json.dumps(company_raw)
+    except Exception as e:
+        return HttpResponse("INNER ERROR"+str(e))
+    return HttpResponse(json_res)
+
+
+def get_company_detail(request):
+    if request.user.is_authenticated:
+        pass
+    else:
+        return HttpResponse("USER NOT LOGIN")
